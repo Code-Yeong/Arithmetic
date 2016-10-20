1351b7721e03218333b1f82771f1fcf6f15ab337
hhhouwt <hhhouwt@163.com>
2016-10-20 22:41:33 +0800

    Revert "grade one and grade two problems generation"

--- Arithmetic-master/app/src/main/java/com/example/administrator/arithmetic_master/grade/GradeFour.java
+++ Arithmetic-master/app/src/main/java/com/example/administrator/arithmetic_master/grade/GradeFour.java
@@ -1,39 +1,8 @@
 package com.example.administrator.arithmetic_master.grade;
 
 
-import java.util.Random;
-
 import com.example.administrator.arithmetic_master.baseimpl.BaseGeneratorImpl;
 
 public class GradeFour extends BaseGeneratorImpl {
-	public String getIntergralExpression(){
-		String reg="";
-		double d = new Random().nextDouble();
-        if (d <= 0.5){
-        	if(d<=0.15){
-        		reg = xiaoshuyunsuan(200);	
-        	}else{
-        		reg = zhengshizhengchu(1,200);
-        	}
-        }else if (d > 0.5 && d <= 0.75){
-        	String str = JiaJian();
-        	if(d<=0.6){
-        		reg = xiaoshuyunsuan(500)+str+xiaoshu(100,3);
-        	}else if(d>0.6 && d<=0.7){
-        		reg = zhengshizhengchu(1,100)+str+xiaoshu(100,3);
-        	}else{
-        		reg = zhengshu(1,15)+"*"+zhengshizhengchu(1,100);
-        	}
-        }else{
-        	if(d<=0.85){
-        		reg = xiaoshuyunsuan(500)+"+"+xiaoshuyunsuan(500);
-        	}else if(d>0.85 && d<=0.9){
-        		reg = zhengshizhengchu(1,100)+"+"+xiaoshuyunsuan(100);
-        	}else{
-        		reg = zhengshizhengchu(1,100)+"+"+zhengshizhengchu(1,100);
-        	}
-        }
-        return reg;
-	}
 
 }
--- Arithmetic-master/app/src/main/java/com/example/administrator/arithmetic_master/grade/GradeThree.java
+++ Arithmetic-master/app/src/main/java/com/example/administrator/arithmetic_master/grade/GradeThree.java
@@ -1,50 +1,8 @@
 package com.example.administrator.arithmetic_master.grade;
 
 
-import java.util.Random;
-
 import com.example.administrator.arithmetic_master.baseimpl.BaseGeneratorImpl;
 
 public class GradeThree extends BaseGeneratorImpl {
 
-	public String getIntergralExpression(){
-		String reg;
-        double d = new Random().nextDouble();
-        if (d <= 0.3){
-        	reg = zhengshizhengchu(1,40);
-        	
-        }else if (d > 0.3 && d <= 0.6){
-        	String str = zhengshizhengchu(1,30);
-        	if(str.contains("*")){
-        		if(d > 0.3 && d <= 0.4){
-        			reg = str+"+"+zhengshu(10,100);
-        		}else if(d > 0.4 && d <= 0.5){
-        			reg = str +"*"+zhengshu(1,30);
-        		}else{
-        			reg = zhengshu(0,1000)+"+"+str;
-        		}
-        	}else{
-        		if(d > 0.3 && d <= 0.4){
-        			reg = zhengshu(0,1000)+"+"+str;
-        		}else if(d > 0.4 && d <= 0.5){
-        			reg = zhengshu(0,30)+"*"+str;
-        		}else{
-        			reg = str+"+"+zhengshu(1,100);
-        		}
-        	}
-        	
-        }else{
-        	if(d <= 0.75){
-        		String str = zhengshizhengchu(1,20);
-        		if(str.contains("*")){
-        			reg = str+"*"+zhengshizhengchu(1,20);
-        		}else{
-        			reg = str+"+"+zhengshizhengchu(1,50);
-        		}
-        	}else{
-        		reg = zhengshizhengchu(1,30)+"+"+zhengshizhengchu(1,30);
-        	}
-        }
-        return reg;
-    }
 }
